9812279c3064986f4d0b0e8d03533619b014ae00
pvin <praaveenranganathan@gmail.com>
2014-03-17 00:31:31 +0530

    chapter 7 commit, finish user signup

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '3.2.13'
+gem 'rails', '4.0'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -21,8 +21,8 @@ end
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'sass-rails',   '~> 3.2.3'
-  gem 'coffee-rails', '~> 3.2.1'
+  #gem 'sass-rails'
+  gem 'coffee-rails'
 
   gem 'bootstrap-sass', '2.3.2.0'
 
@@ -32,10 +32,11 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
+gem 'sass-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'turbolinks', '1.1.1'
 gem 'jbuilder', '1.0.2'
-gem 'bcrypt-ruby', '3.0.0'
+gem 'bcrypt-ruby'
 group :doc do
   gem 'sdoc', '0.3.20', require: false
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
+  #get "users/new"
   get "static_pages/home"
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
@@ -9,6 +10,7 @@ SampleApp::Application.routes.draw do
   #get "static_pages/about"
   #get "static_pages/contact"
   get "test1/t"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -58,7 +60,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
