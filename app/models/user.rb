bd82afaefd786909bd73e1d5eeaf7d642200b639
pvin <praaveenranganathan@gmail.com>
2014-04-09 15:02:17 +0530

    end of chapter 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
 
   #attr_accessible :email, :name, :password, :password_confirmation, :password_digest
   validates :name, presence: true, length: { minimum:5 , maximum: 50 }
@@ -7,7 +8,22 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
              format: { with: VALID_EMAIL_REGEX },
              uniqueness: { case_sensitive: false }
-  #has_secure_password
+  has_secure_password
   validates :password, length: { minimum: 6 }
+
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.hash(User.new_remember_token)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/sessions',  to: 'sessions#create',            via: 'get'
   #get "users/new"
   get "static_pages/home"
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+
   #get "static_pages/help"
   #get "static_pages/about"
   #get "static_pages/contact"
